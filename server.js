09f73d6029a059395770bcfd044602ad880af877
adarshreddy87 <sparkadarshreddy@gmail.com>
2021-04-12 17:34:50 -0400

    Created node.js server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+// const demos =require('./controllers/demos-controller')
+// demos(app)
+
+const quizzesController = require("./controllers/quizzes_controller")
+const questionController = require("./controllers/question_controller")
+quizzesController(app)
+questionController(app)
+app.listen(3000)
